7f0739c2bd2d0e4dda1984eaa5324e6194b4f53c
clement <ccaylu@gmail.com>
2019-03-02 10:41:55 +0000

    code comments

--- react-component/src/index.js
+++ react-component/src/index.js
@@ -5,6 +5,8 @@ import ClassComponent from './ClassComponent';
 import {StatelessComponent} from './StatelessComponent';
 import * as serviceWorker from './serviceWorker';
 
+
+// the next render the react components in the dom either as a class component or as a function component. Comment out one and uncomment the other one to see the difference ( It should not be much :-))
 // ReactDOM.render(<ClassComponent />, document.getElementById('root'));
 ReactDOM.render(StatelessComponent() , document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
